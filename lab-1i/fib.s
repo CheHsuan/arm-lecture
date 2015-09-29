2845a9fa331112aafdb7c8d6c055d20c46617379
CheHsuan <exas60807@gmail.com>
2015-09-29 14:05:00 +0800

    Submit the first iterative version

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -11,33 +11,32 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r3, r4, r5, lr}
-
-	@ R4 = R0 - 0 (update flags)
-	@ if(R0 <= 0) goto .L3 (which returns 0)
-
-	@ Compare R4 wtih 1
-	@ If R4 == 1 goto .L4 (which returns 1)
-
-	@ R0 = R4 - 1
-	@ Recursive call to fibonacci with R4 - 1 as parameter
-
-	@ R5 = R0
-	@ R0 = R4 - 2
-	@ Recursive call to fibonacci with R4 - 2 as parameter
-
-	@ R0 = R5 + R0 (update flags)
-
-	pop {r3, r4, r5, pc}		@EPILOG
+	push {r4, r5, r6, r7, lr}
+	@r4 = -1 (previous)
+	mov r4 , #0xFFFFFFFF
+	@r5 = 1 (result)
+	mov r5 , #1
+	@r6 = 0 (i)
+	mov r6, #0
+	@r7 = 0 (sum)
+	mov r7, #0
+	@loop, compare r6 with r0
+	cmp r6, r0
+	ble .Loop
+	mov r0, r5
+	
+	pop {r4, r5,r6, r7, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
-.L3:
-	mov r0, #0			@ R0 = 0
-	pop {r3, r4, r5, pc}		@ EPILOG
-
-.L4:
-	mov r0, #1			@ R0 = 1
-	pop {r3, r4, r5, pc}		@ EPILOG
+.Loop:
+	add r6, r6, #1
+	add r7, r4, r5	
+	mov r4, r5
+	mov r5, r7
+	cmp r6, r0
+	ble .Loop
+	mov r0, r5
+	pop {r4, r5, r6,r7, pc}	
 
 	.size fibonacci, .-fibonacci
 	.end
