b04ee913531954d7e9706c3af0436297410017bd
CheHsuan <exas60807@gmail.com>
2015-09-29 17:26:19 +0800

    The second iterativ version-Shorten the code and eliminate the usage of register
    * Shorten the code in .Loop by using conditional excution(subs and branch)
    * Eliminate the usage of register(4->3)

--- lab-1i/fib.s
+++ lab-1i/fib.s
@@ -11,32 +11,31 @@
 fibonacci:
 	@ ADD/MODIFY CODE BELOW
 	@ PROLOG
-	push {r4, r5, r6, r7, lr}
+	push {r4, r5, r6, lr}
 	@r4 = -1 (previous)
-	mov r4 , #0xFFFFFFFF
+	@mov r4 , #0xFFFFFFFF
+	mvn r4, #0x0
 	@r5 = 1 (result)
 	mov r5 , #1
-	@r6 = 0 (i)
+	@r6 = 0 (sum)
 	mov r6, #0
-	@r7 = 0 (sum)
-	mov r7, #0
-	@loop, compare r6 with r0
-	cmp r6, r0
-	ble .Loop
+	@loop, compare r0 with #0
+	add r0 , r0, #1
+	subs r0, r0, #1
+	bge .Loop
 	mov r0, r5
 	
-	pop {r4, r5,r6, r7, pc}		@EPILOG
+	pop {r4, r5,r6, pc}		@EPILOG
 
 	@ END CODE MODIFICATION
 .Loop:
-	add r6, r6, #1
-	add r7, r4, r5	
-	mov r4, r5
-	mov r5, r7
-	cmp r6, r0
-	ble .Loop
+	add r6, r4, r5 @sum = result+previous	
+	mov r4, r5 @previous = result
+	mov r5, r6 @result = sum
+	subs r0, r0, #1
+	bge .Loop
 	mov r0, r5
-	pop {r4, r5, r6,r7, pc}	
+	pop {r4, r5, r6, pc}	
 
 	.size fibonacci, .-fibonacci
 	.end
